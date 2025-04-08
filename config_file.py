be449e7d1b65291230241cbd997108e020899a72
sugarcandy <40589172+sugarcandy@users.noreply.github.com>
2025-04-08 14:04:05 +0800

    Update config_file.py

--- config_file.py
+++ config_file.py
@@ -1,13 +1,13 @@
 config = {}
-config['seed'] = [2023]  # Pheme为2023，原始为123，我们的是2023
-config['epochs'] = [60]  # weibo为20，pheme是60
+config['seed'] = [2023] 
+config['epochs'] = [60]
 config['batch_size'] = [64]
 config['use_stopwords'] = [True]
 config['maxlen'] = [50]
 config['ratio'] = [[70, 10, 20]]
 config['kernel_sizes'] = [[3, 4, 5]]
 config['dropout'] = [0.6]
-config['user_self_attention'] = [False]  # pheme为False
+config['user_self_attention'] = [False]
 config['n_heads'] = [8]
 config['nb_heads'] = [4]
 config['num_classes'] = [2]
